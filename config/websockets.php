61b3118c3e9f76326b62d4e392982ed7b7e5f4ec
AadiMalik <adnanjunaid76279@gmail.com>
2022-08-20 10:18:37 +0500

    20082022

--- config/websockets.php
+++ config/websockets.php
@@ -104,6 +104,7 @@ return [
      * You can see all available options at: http://php.net/manual/en/context.ssl.php
      */
     'ssl' => [
+        'verify_peer'=>false,
         /*
          * Path to local certificate file on filesystem. It must be a PEM encoded file which
          * contains your certificate and private key. It can optionally contain the
@@ -122,6 +123,7 @@ return [
          * Passphrase for your local_cert file.
          */
         'passphrase' => env('LARAVEL_WEBSOCKETS_SSL_PASSPHRASE', null),
+        
     ],
 
     /*
